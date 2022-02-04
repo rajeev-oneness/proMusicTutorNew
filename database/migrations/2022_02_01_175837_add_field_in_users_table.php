1e5aae6a1c6f2afcc5f8b173d56990d590afa7a6
suvajit-oneness <suvajit.bardhan@onenesstechs.in>
2022-02-04 15:45:03 +0530

    user table migration update

--- database/migrations/2022_02_01_175837_add_field_in_users_table.php
+++ database/migrations/2022_02_01_175837_add_field_in_users_table.php
@@ -14,7 +14,7 @@ class AddFieldInUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('description')->after('aniversary')->nullable();
+            $table->taxt('description')->after('aniversary')->nullable();
         });
     }
 
